fea447548d901bb49fcbd0430c510ca280dec597
vickanS <149102936+vickanS@users.noreply.github.com>
2024-12-04 17:57:09 +0100

    Added Route to customer login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react"; // Lägg till denna rad
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./pages/Customer/Profile";
+import Login from "./pages/Customer/Login";
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/customer/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
