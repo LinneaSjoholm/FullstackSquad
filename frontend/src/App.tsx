31b17e042852ddc09ce5208543f844ae115d2668
vickanS <149102936+vickanS@users.noreply.github.com>
2024-12-05 15:26:34 +0100

    Added an import and a route to Create Account

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import React from "react"; // Lägg till denna rad
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./pages/Customer/Profile";
 import Login from "./pages/Customer/Login";
+import CreateAccount from "./pages/Customer/CreateAccount";
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/customer/create-account" element={<CreateAccount />} />
         <Route path="/customer/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
