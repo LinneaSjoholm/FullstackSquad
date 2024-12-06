8686e71c88f64b2311db3502dcd94a6594d8fac3
vickanS <149102936+vickanS@users.noreply.github.com>
2024-12-06 18:11:12 +0100

    Added import and Route to the admin login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./pages/Customer/Profile";
 import Login from "./pages/Customer/Login";
 import CreateAccount from "./pages/Customer/CreateAccount";
+import LoginAdmin from "./pages/Employed/Login";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/customer/create-account" element={<CreateAccount />} />
-        <Route path="/customer/login" element={<Login />} />
+        <Route path="/user/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/admin/login" element={<LoginAdmin />} />
       </Routes>
     </Router>
   );
