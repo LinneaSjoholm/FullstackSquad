ef4d87421e9f3271ea2ba821498190f5698a95f0
LinneaSjoholm <linnea.sjoholm@live.se>
2024-12-09 14:35:23 +0100

    removed comment from db.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,9 +7,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/order': {
-        target: 'https://3uhcgg5udg.execute-api.eu-north-1.amazonaws.com', // Din API-server
-        changeOrigin: true, // För att byta origin till serverns domän
-        rewrite: (path) => path.replace(/^\/order/, ''), // Ändra path om det behövs
+        target: 'https://3uhcgg5udg.execute-api.eu-north-1.amazonaws.com',
+        changeOrigin: true, 
+        rewrite: (path) => path.replace(/^\/order/, ''),
       },
     },
   },
