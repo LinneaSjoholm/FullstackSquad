4594302f20ce6e80428661c5daa23ccaaa01ca25
LinneaSjoholm <linnea.sjoholm@live.se>
2024-12-09 17:08:58 +0100

    had issues in .yml that had to be fixed

--- backend/src/services/db.ts
+++ backend/src/services/db.ts
@@ -3,7 +3,6 @@ import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
 
 // Skapa DynamoDB-klienten
 const client = new DynamoDB({
-<<<<<<< HEAD
   region: process.env.AWS_REGION, 
 });
 
@@ -12,12 +11,3 @@ const db = DynamoDBDocument.from(client);
 const docClient = DynamoDBDocument.from(client);
 
 export { db, docClient }; 
-=======
-  region: process.env.AWS_REGION,  // Se till att du har rätt AWS-region definierad
-});
-
-// Skapa och exportera en instans av DynamoDBDocument
-const db = DynamoDBDocument.from(client);
-
-export { db };  // Exports for use in other filesw
->>>>>>> Lam
