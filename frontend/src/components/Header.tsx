89351f966846d43f57f0ba600b2eb3f325a76b18
Lamskidoski <lam.ky@hotmail.com>
2024-12-10 19:27:39 +0700

    done with About, fixed the header logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,8 +5,8 @@ import '../styles/Header.css';
 const Header: React.FC = () => {
   return (
     <header className="header">
-      
-      <h1>Gusto To Go</h1>
+      <h1>Gusto</h1>
+      <h2>To Go</h2>
       {/* Här kan du lägga till meny, ikon eller andra element */}
     </header>
   );
